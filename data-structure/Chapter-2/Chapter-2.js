419080b0a6535f0c32b56b368085b1f2800c1584
liuxilei <liuxileioo@qq.com>
2018-04-10 09:48:34 +0800

    配置项删除

--- data-structure/Chapter-2/Chapter-2.js
+++ data-structure/Chapter-2/Chapter-2.js
@@ -32,3 +32,15 @@ console.log('数组删除方法:pop和shift');
 //push和pop可以模仿栈
 //shift删除元素第一个元素
 //shift和unshift可以模仿队列
+numbers.shift();
+console.log(numbers);  //[9, 9, 0, 1, 3, 4, 4, 6, 7]
+numbers.pop();
+console.log(numbers);  //[9, 9, 0, 1, 3, 4, 4, 6]
+//splice方法参数一为截取索引位置开头部分，参数二表示截取元素个数
+numbers.splice(3,2);
+console.log(numbers);  //[9, 9, 0, 4, 4, 6]
+//再把删除的[1,3]重新插入进去
+numbers.splice(3,0,1,3);
+console.log(numbers);  //[9, 9, 0, 1, 3, 4, 4, 6]
+numbers.splice(5,3,2,3,4);
+console.log(numbers);  //[9, 9, 0, 1, 3, 2, 3, 4] 从第六位数字开始截取，截取3个数字，然后用[2，3，4]从索引为5的地方插入
